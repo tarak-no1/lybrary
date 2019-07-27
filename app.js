08fe653c9d706f4a51b8dbd353ed7708a71504bf
tarak <456tarakesh@gmail.com>
2019-07-28 01:50:00 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -14,8 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/v1/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
